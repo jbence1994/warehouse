970c53bef02c828944c1b2efc796e9c7e7bfc58e
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-04-10 14:38:36 +0200

    Register AutoMapper.

---
+++ Mapping/MappingProfile.cs
@@ -0,0 +1,11 @@
+using AutoMapper;
+
+namespace Warehouse.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
@@ -21,6 +22,8 @@ namespace Warehouse
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper();
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseMySQL(Configuration.GetConnectionString("Default")));
 
