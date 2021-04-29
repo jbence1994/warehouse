f4a26c6e349c9900bea6113366418e9495114e11
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-04-29 13:02:53 +0200

    Using extract method refactor in checkout method in sale service.

--- Persistence/Services/SaleService.cs
+++ Persistence/Services/SaleService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Warehouse.Core;
@@ -33,18 +34,37 @@ namespace Warehouse.Persistence.Services
 
         public async Task<Sale> Checkout(Sale sale)
         {
-            foreach (var saleDetail in sale.SaleDetails)
+            await CalculateSaleDetailSubTotals(sale.SaleDetails);
+            CalculateSaleTotal(sale);
+            await AssignSaleToTechnician(sale);
+            await DecrementProductQuantityInStockSummary(sale.SaleDetails);
+            await CompleteSale();
+
+            return sale;
+        }
+
+        private async Task CalculateSaleDetailSubTotals(ICollection<SaleDetail> saleDetails)
+        {
+            foreach (var saleDetail in saleDetails)
             {
                 var product = await productRepository.GetProduct(saleDetail.ProductId);
                 saleDetail.SubTotal = product.Price * saleDetail.Quantity;
             }
+        }
 
+        private void CalculateSaleTotal(Sale sale)
+        {
             var total = sale.SaleDetails.Sum(s => s.SubTotal);
             sale.Total = total;
+        }
 
+        private async Task AssignSaleToTechnician(Sale sale)
+        {
             var technician = await technicianRepository.GetTechnician(sale.TechnicianId);
-            technician.Balance.Amount -= total;
+
             technician.Sales.Add(sale);
+
+            technician.Balance.Amount -= sale.Total;
 
             await technicianBalanceRepository.Add(new TechnicianBalance
             {
@@ -52,28 +72,34 @@ namespace Warehouse.Persistence.Services
                 Amount = technician.Balance.Amount,
                 CreatedAt = DateTime.Now
             });
+        }
 
+        private async Task DecrementProductQuantityInStockSummary(ICollection<SaleDetail> saleDetails)
+        {
             var summarizedStocks = await stockRepository.GetSummarizedStocks();
 
-            foreach (var saleDetail in sale.SaleDetails)
+            foreach (var saleDetail in saleDetails)
             {
                 foreach (var stockSummary in summarizedStocks)
                 {
-                    if (saleDetail.ProductId == stockSummary.ProductId)
+                    if (saleDetail.ProductId != stockSummary.ProductId)
                     {
-                        if (stockSummary.Quantity <= 0 || stockSummary.Quantity < saleDetail.Quantity)
-                        {
-                            throw new Exception("There is not enough product on stock to sell.");
-                        }
-
-                        stockSummary.Quantity -= saleDetail.Quantity;
+                        continue;
                     }
+
+                    if (stockSummary.Quantity <= 0 || stockSummary.Quantity < saleDetail.Quantity)
+                    {
+                        throw new Exception("There is not enough product on stock to sell.");
+                    }
+
+                    stockSummary.Quantity -= saleDetail.Quantity;
                 }
             }
+        }
 
+        private async Task CompleteSale()
+        {
             await unitOfWork.CompleteAsync();
-
-            return sale;
         }
     }
 }
