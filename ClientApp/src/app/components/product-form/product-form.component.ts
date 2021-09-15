3addf37c09e06810cec59b535939cdc65fe0f8de
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-09-15 19:59:14 +0200

    Implement product posting to server

--- ClientApp/src/app/components/product-form/product-form.component.ts
+++ ClientApp/src/app/components/product-form/product-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 import ProductService from "./../../services/product.service";
 import SaveProduct from "src/app/models/saveProduct";
 import KeyValuePair from "./../../models/keyValuePair";
@@ -17,14 +18,15 @@ export default class ProductFormComponent implements OnInit {
     supplierId: 0,
   };
 
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService, private router: Router) {}
 
   ngOnInit() {
     this.populateSuppliers();
   }
 
   submit() {
-    console.log("POST request to REST API ...");
+    this.productService.createProduct(this.saveProduct).subscribe();
+    this.router.navigate(["/termeklista/"]);
   }
 
   populateSuppliers() {
