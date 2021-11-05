ebfa6ff5ea511389b27d4339181e9b1388affae4
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-11-05 19:07:40 +0100

    Seed technician balance entries table with initial data

---
+++ Migrations/20211105180407_SeedTechnicianBalanceEntriesTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Warehouse.Migrations
+{
+    public partial class SeedTechnicianBalanceEntriesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "INSERT INTO technician_balance_entries (technician_id, amount, created_at) VALUES ((SELECT id FROM technicians WHERE last_name = 'Papp'), 0, CURRENT_TIMESTAMP());");
+            migrationBuilder.Sql(
+                "INSERT INTO technician_balance_entries (technician_id, amount, created_at) VALUES ((SELECT id FROM technicians WHERE last_name = 'Ádók'), 0, CURRENT_TIMESTAMP());");
+            migrationBuilder.Sql(
+                "INSERT INTO technician_balance_entries (technician_id, amount, created_at) VALUES ((SELECT id FROM technicians WHERE last_name = 'Lengyel'), 0, CURRENT_TIMESTAMP());");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM technician_balance_entries;");
+        }
+    }
+}
