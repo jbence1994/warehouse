04093960b28d71dac64714708fbaa2b5c76a59ae
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-11-07 11:52:24 +0100

    Clean up code

--- Startup.cs
+++ Startup.cs
@@ -68,7 +68,7 @@ namespace Warehouse
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
