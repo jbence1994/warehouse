3a5d1bfdf8417681460a6e2c168ae72af6c5fa16
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-12-01 12:19:43 +0100

    Rename controller endpoint

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -11,7 +11,7 @@ using Warehouse.Services.Exceptions;
 namespace Warehouse.Controllers
 {
     [ApiController]
-    [Route("api/[controller]/")]
+    [Route("/api/v1/[controller]/")]
     public class ProductsController : ControllerBase
     {
         private readonly ProductService _productService;
