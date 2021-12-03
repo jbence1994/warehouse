6b77bdf8dd3e2341ceb54327728b10466ac7bd72
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-12-03 13:32:31 +0100

    Rename endpoint

--- Controllers/MerchantsController.cs
+++ Controllers/MerchantsController.cs
@@ -11,7 +11,7 @@ using Warehouse.Core.Repositories;
 namespace Warehouse.Controllers
 {
     [ApiController]
-    [Route("api/[controller]/")]
+    [Route("/api/v1/[controller]/")]
     public class MerchantsController : ControllerBase
     {
         private readonly IMerchantRepository _merchantRepository;
