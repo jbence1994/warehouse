2bca73d3b25ae63f18a065e6125a26064cd2f3ff
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-12-03 13:34:55 +0100

    Inject dependencies

--- Services/MerchantService.cs
+++ Services/MerchantService.cs
@@ -1,6 +1,20 @@
-﻿namespace Warehouse.Services
+﻿using Warehouse.Core;
+using Warehouse.Core.Repositories;
+
+namespace Warehouse.Services
 {
     public class MerchantService
     {
+        private readonly IMerchantRepository _merchantRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public MerchantService(
+            IMerchantRepository merchantRepository,
+            IUnitOfWork unitOfWork
+        )
+        {
+            _merchantRepository = merchantRepository;
+            _unitOfWork = unitOfWork;
+        }
     }
 }
